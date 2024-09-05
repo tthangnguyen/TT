71962c678dba749abce0fe2f00686d4d5bd96882
tthangnguyen <tthangga502@gmail.com>
2024-09-05 22:55:22 +0700

    sua file bai tap

---
+++ BaiTap02/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap03.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

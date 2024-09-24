7e4f4013616a2d2ed0752e27160af40701791791
tthangnguyen <tthangga502@gmail.com>
2024-09-24 15:38:19 +0700

    BaiKiemTra02

---
+++ BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TenLopHoc { get; set; }
+        public DateTime NamNhapHoc { get; set; } = DateTime.Now;
+        public DateTime NamRaTruong { get; set; } = DateTime.Now;
+        public int SoLuongSinhVien { get; set; }
+    }
+}
